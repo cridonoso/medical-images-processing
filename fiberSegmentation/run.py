06d4afcadd511bafd8d13218c1a4bdb0121b55a0
Cristobal <cridonoso@udec.cl>
2018-06-14 20:46:34 -0400

    final

--- fiberSegmentation/run.py
+++ fiberSegmentation/run.py
@@ -1,69 +1,64 @@
-
 # coding: utf-8
-
-# In[5]:
-
-
 from read_write_bundles import *
-import tensorflow as tf
 import numpy as np
-from joblib import Parallel, delayed
-import multiprocessing
 from time import time
+from scipy.spatial import distance
+#For parallel
+import multiprocess as mp
 
 
-# In[24]:
-
-
-path = 'atlas_faisceaux_MNI/atlas_AR_ANT_LEFT_MNI.bundles'
-path2 = 'whole_brain_MNI_100k_21p.bundles'
-whole_brain = read_bundle(path2)
-
-object_ = read_bundle(path)
-object_ = object_[0:2]
-
-
-# In[25]:
-
-
-def calculate_distance(fiber1, fiber2, umbral):
-    distances = []
+def calculate_distance(*args):
+    dies = []
+    fiber1 = args[0][0]
+    fiber2 = args[0][1]
+    umbral = args[0][2]
     for k in range(0, fiber1.shape[0]):
-        session  = tf.Session()
-        distance = tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(fiber1[k],fiber2[k]))))
-        d = session.run(distance)
-        if d > umbral:
-            return d
+        d = distance.euclidean(fiber1[k], fiber2[k])
+        if d <= umbral:
+            dies.append(d)
         else:
-            distances.append(d)
-    return np.max(distances)
+            return d, False
+    return (dies, fiber2), True
 
 
-fiber = []
-t0 = time()
-r1s = []
-for o in object_:
-    num_cores = multiprocessing.cpu_count()
-    r = Parallel(n_jobs=num_cores)(delayed(calculate_distance)(o,w,9) 
-                                  for w in whole_brain)
-    r1s.append(r)
-
-r1s = np.array(r1s)  
-r2s = []
-for o in object_:
-    num_cores = multiprocessing.cpu_count()
-    r2 = Parallel(n_jobs=num_cores)(delayed(calculate_distance)(o,w[::-1],9) 
-                                  for index,(w) in enumerate(whole_brain))
-    r2s.append([r2, index])
-
-r2s = np.array(r2s)
-for i in range(0, r1s.shape[0]):   
-    minimum = min(r1s[i][0],r2s[i][0])
-    if minimum <= 9:
-        fiber.append(r2s[i][1])
-
-t1 = time()
-
-print ('Algorithms takes %f' %(t1-t0))
+if __name__ == '__main__':
+    path = 'atlas_faisceaux_MNI/atlas_AR_ANT_LEFT_MNI.bundles'
+    path2 = 'whole_brain_MNI_100k_21p.bundles'
+    whole_brain = read_bundle(path2)
+    object_ = read_bundle(path)
 
 
+    parallel = False
+    max_num = 10000
+    whole_brain = whole_brain[0:max_num]
+    print 'object: ',len(object_)
+    print 'whole_brain: ', len(whole_brain)
+    if not parallel:
+        print 'Running with Simple Computing'
+        t0 = time()
+        distances_new = []
+        for o in object_:
+            for w in whole_brain:
+                dist, save = calculate_distance((o,w[::-1],9))
+                if save:
+                    distances_new.append(dist[1])
+                    print dist[1]
+
+
+        t1 = time()
+        print ('Algorithms takes %f' %(t1-t0))
+    else:
+        print 'Running in Parallel Computing'
+        p = mp.Pool()
+        t0 = time()
+        input = [(o, w[::-1], 9) for o in object_ for w in whole_brain]
+        results = p.map(calculate_distance, input)
+        p.close()
+        p.join()
+
+        for r in results:
+            if r[1]:
+                print r[0]
+
+        t1 = time()
+        print ('Algorithms takes %f' %(t1-t0))
